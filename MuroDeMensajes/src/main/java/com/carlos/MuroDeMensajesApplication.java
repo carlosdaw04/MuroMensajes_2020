5853e0dabdd9b0c12d38b0056cb5ae839b77a4e1
Carlos <Carlos@DESKTOP-Q51GSC7>
2020-01-21 09:27:29 +0100

    Iniciando el proyecto

---
+++ MuroDeMensajes/src/main/java/com/carlos/MuroDeMensajesApplication.java
@@ -0,0 +1,13 @@
+package com.carlos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuroDeMensajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuroDeMensajesApplication.class, args);
+	}
+
+}
