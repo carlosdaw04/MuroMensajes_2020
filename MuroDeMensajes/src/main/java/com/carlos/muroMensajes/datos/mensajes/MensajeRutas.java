7fef30ae06635d46800c987a0702fbe8c5e2eb19
Carlos <Carlos@DESKTOP-Q51GSC7>
2020-01-22 11:31:37 +0100

    Añadir y borrar mensajes de la base de datos desde la vista.

--- MuroDeMensajes/src/main/java/com/carlos/muroMensajes/datos/mensajes/MensajeRutas.java
+++ MuroDeMensajes/src/main/java/com/carlos/muroMensajes/datos/mensajes/MensajeRutas.java
@@ -1,54 +1,63 @@
 package com.carlos.muroMensajes.datos.mensajes;
 
 import java.util.List;
-import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MensajeRutas {
-	
+
 	@Autowired
 	private MensajeDAO mensajeDAO;
-	
 
 	@GetMapping("/mensajes")
 	public ModelAndView rutaInicial() {
-		
+
 		ModelAndView model = new ModelAndView();
 		model.setViewName("mensajes");
-		
+		model.addObject("mensaje", new Mensaje());
+
 		List<Mensaje> listaMensajes = (List<Mensaje>) mensajeDAO.findAll();
 		model.addObject("mensajes", listaMensajes);
-		
+
 		return model;
 	}
 	
+	@PostMapping("/mensajes/anadir")
+	public String rutaAnadir(@ModelAttribute Mensaje mensaje) {
+
+		mensajeDAO.save(mensaje);
+
+		return "redirect:/mensajes";
+	}
+
 	@GetMapping("/mensajes/{id}")
 	public ModelAndView rutaEscribir() {
-		
+
 		ModelAndView model = new ModelAndView();
 
 		return model;
 	}
-	
+
 	@GetMapping("/mensajes/borrar/{id}")
-	public ModelAndView rutaBorrar() {
+	public String rutaEliminar(@PathVariable long id) {
+		//Version 1
+//		Mensaje mensaje = mensajeDAO.findById(id).get();
+//		mensajeDAO.delete(mensaje);
 		
-		ModelAndView model = new ModelAndView();
-
-		return model;
-	}
-	
-	@GetMapping("/mensajes/anadir")
-	public ModelAndView rutaVisualizar() {
+		//Version 2
+		mensajeDAO.deleteById(id);
 		
-		ModelAndView model = new ModelAndView();
+		return("redirect:/mensajes");
 
-		return model;
 	}
 
 }
