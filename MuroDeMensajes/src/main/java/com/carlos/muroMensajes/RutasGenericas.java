d5942af506eeaf40b6bf7d563fd954503c592772
Carlos <Carlos@CarloMSI>
2020-02-12 13:19:16 +0100

    Cambios en la seguridad.
    Añadida una vista para el login.

--- MuroDeMensajes/src/main/java/com/carlos/muroMensajes/RutasGenericas.java
+++ MuroDeMensajes/src/main/java/com/carlos/muroMensajes/RutasGenericas.java
@@ -1,9 +1,5 @@
 package com.carlos.muroMensajes;
 
-import java.util.List;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -12,13 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class RutasGenericas {
 
 	@GetMapping("/")
-	public String inicial(Authentication authentication) {
-		
-		System.out.println(authentication.isAuthenticated());
-		System.out.println(authentication.getName());
-		List<GrantedAuthority> permisos = (List<GrantedAuthority>)authentication.getAuthorities();
+	public String inicial() {
 
 		return "index";
+	}
+	
+	@GetMapping("/login")
+	public String seguridad() {
+		
+		return "start";
 	}
 	
 }
--- MuroDeMensajes/src/main/java/com/carlos/muroMensajes/seguridad/SecurityConfig.java
+++ MuroDeMensajes/src/main/java/com/carlos/muroMensajes/seguridad/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -28,5 +29,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	    	auth.authenticationProvider(provider);
 	    }
 
+	    @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	    	http
+	        .authorizeRequests()
+	        	.antMatchers("/usuarios").permitAll()
+	        	.antMatchers("/mensajes/**").authenticated()
+	        	.antMatchers("/usuarios/**").hasAuthority("ADMIN")
+	        	.antMatchers("/usuarios/anadir").hasAnyAuthority("ADMIN","MODERADOR")
+		        .and()    	
+	        .formLogin()
+	        	.loginPage("/login")
+	            .defaultSuccessUrl("/")
+	            .failureUrl("/login?error=true")
+	            .usernameParameter("username")
+	            .passwordParameter("password")
+	            .and()
+	        .logout()
+	        	.permitAll()
+	            .logoutUrl("/logout")
+	            .logoutSuccessUrl("/login")
+	            .and()
+	        .csrf().disable();
+
+	    }
 	
 }
